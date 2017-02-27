46b9b3f8fb8665e7c48395038a38d7a1fdaab96c
Mark Bellingham <mark.bellingham@protonmail.ch>
2017-02-27 15:07:23 +0000

    moved Get Page Hash functions to own class

--- src/project_web_sec/Controller.java
+++ src/project_web_sec/Controller.java
@@ -1,18 +1,16 @@
 package project_web_sec;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class Controller {
 
-	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+	public static void main(String[] args) {
 		
 		// Create an instance of the DAO in order to use its methods
 		DAO dao = new DAO();
+		GetPageHash getPageHash = new GetPageHash();
 		
 		// Create an ArrayList of all the pages to be monitored
 		ArrayList<String> addresses = new ArrayList<String>();
@@ -23,32 +21,18 @@ public class Controller {
 		addresses.add("http://localhost/ProjectWebSec/css/stylesheet.css");
 		
 		// Loop through the pages
-		for(String address: addresses) {
+		for(String address : addresses) {
 			
-			// Create a URL from the String and print it to the console for troubleshooting
-			URL url = new URL(address);
-			System.out.println(url);
-			
-			// Use BufferedReader to read the stream of data from the network
-			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-			
-			// Convert the data stream in the BufferedReader to a String
-			String input, pageSource = "";
-			while ((input = br.readLine()) != null) {
-				pageSource += input;
+			String sha512 = null;
+			try {
+				sha512 = getPageHash.get(address);
+			} catch (NoSuchAlgorithmException | IOException e) {
+				e.printStackTrace();
 			}
-			br.close();
-			
-			// Get a hash value form the page source
-			String sha512 = HashFunction.hash(pageSource);
-			
-			// Print the hash value for troubleshooting
-			System.out.println(sha512);
 			
 			// Create a new page object and insert it into the database
 			Page page = new Page(address, sha512);
 			dao.insertPage(page);
 		}
 	}
-
 }
---
+++ src/project_web_sec/GetPageHash.java
@@ -0,0 +1,35 @@
+package project_web_sec;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.security.NoSuchAlgorithmException;
+
+public class GetPageHash {
+	
+	public String get(String address) throws IOException, NoSuchAlgorithmException {
+		
+			// Create a URL from the String and print it to the console for troubleshooting
+			URL url = new URL(address);
+			System.out.println(url);
+			
+			// Use BufferedReader to read the stream of data from the network
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+			// Convert the data stream in the BufferedReader to a String
+			String input, pageSource = "";
+			while ((input = br.readLine()) != null) {
+				pageSource += input;
+			}
+			br.close();
+			
+			// Get a hash value form the page source
+			String sha512 = HashFunction.hash(pageSource);
+			
+			// Print the hash value for troubleshooting
+			System.out.println(sha512);
+			
+			return sha512;
+	}
+}
